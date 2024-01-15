248e713c2c664bdd84ef249d471d683c6df34e09
musak <musakantar2009@gmail.com>
2024-01-15 19:16:06 +0100

    saving

--- Main.py
+++ Main.py
@@ -8,13 +8,21 @@ import Functionsfortodo as functions
 st.title("My Todo App")
 st.subheader("Hi")
 
-
 todos = functions.get_todos()
+
+def add_todo():
+    newtodo = st.session_state["todo_input"]
+    todos.append(newtodo)
+    functions.write_todos(todos)
+
 
 for i in todos:
     st.checkbox(i)
 
 
-st.text_input(label="nice",placeholder="Add new todo:")
+st.text_input(label="nice",placeholder="Add new todo...",
+              on_change=add_todo, key="todo_input")
 
 print("Bismillah")
+
+st.session_state
